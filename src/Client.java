cfaaf59dbd230ff94c4c20bd90ed12a573becbca
Abhineet Gupta <a.gupta1905@gmail.com>
2017-03-31 08:55:01 +1100

    Added comments; added check for data on streams before reading.

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,44 @@
+/*
+ * Distributed Systems
+ * Group Project 1
+ * Sem 1, 2017
+ * Group: AALT
+ * 
+ * Client-Server Template
+ * AB 
+ */
 
-public class Client {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
+class Client {
+
+    private static String ip = "localhost";
+    private static int port = 3000;
+    
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        System.out.println("Client has started.");
 
+            //Create client socket that auto-closes upon TRY exit
+            //...and connect to a server socket
+        try (Socket socket = new Socket(ip, port)){
+            
+            //Get I/O streams for connection
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            
+            //Write something on outgoing stream and send it through
+            output.writeUTF("I want to connect!");
+            output.flush();
+            
+            //Read anything the client may have sent, and print
+            if (input.available() != 0) {
+                String message = input.readUTF();
+                System.out.println(message);
+            }       
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,62 @@
+/*
+ * Distributed Systems
+ * Group Project 1
+ * Sem 1, 2017
+ * Group: AALT
+ * 
+ * Client-Server Template
+ * AB 
+ */
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.net.ServerSocketFactory;
 
 public class Server {
 
+    private static int counter = 0;     //keep count of number of client connections
+    private static int port = 3000;
+    
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        System.out.println("Server has started.");
+        
+        //factory for server sockets
+        ServerSocketFactory factory = ServerSocketFactory.getDefault();
+        
+        //Create server socket that auto-closes, and bind to port
+        try (ServerSocket server = factory.createServerSocket(port)){
+            System.out.println("Waiting for client connection...");
+            
+            //Keep listening for connections forever
+            while (true){
+                Socket client = server.accept();
+                counter++;
+                System.out.println("Client " + counter + " requesting connection.");
+                
+                //Create, and start, a new thread that processes incoming connections
+                Thread t = new Thread(() -> serveClient(client));
+                t.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
+    
+    private static void serveClient(Socket client) {
+        try (Socket clientSocket = client){
+            //Assign streams for the client connection
+            DataInputStream input = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+            
+            //Echo whatever the client says, and respond to the client
+            if (input.available() != 0){
+                System.out.println("Client: " + input.readUTF());
+                output.writeUTF("Server: Hi Client " + counter + " .");
+            }   
+        } catch (Exception e) {
+            e.printStackTrace();
+        }   
+    }
 }
