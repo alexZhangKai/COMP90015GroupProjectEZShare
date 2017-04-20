af82ccae4a385851ac4f27dbf7356d2119231283
Lisharabbit <lisaqiu763@gmail.com>
2017-04-20 21:35:33 +1000

    Commit before merge from master

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,18 +1,17 @@
-package client;
-
 /*
  * Distributed Systems
  * Group Project 1
  * Sem 1, 2017
  * Group: AALT
- * 
- * Client-Server Template
- * AB 
+ *
+ * Client-side application
  */
+jjjj
+package client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.net.Socket;
 
 import org.apache.commons.cli.CommandLine;
@@ -22,75 +21,257 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Client {
     private static String ip;
     private static int port;
+    private static final Map<String, Boolean> argOptions;
+    static{
+        argOptions = new HashMap<>();
+        argOptions.put("channel", true);
+        argOptions.put("debug", false);
+        argOptions.put("description", true);
+        argOptions.put("exchange", false);
+        argOptions.put("fetch", false);
+        argOptions.put("host", true);
+        argOptions.put("name", true);
+        argOptions.put("owner", true);
+        argOptions.put("port", true);
+        argOptions.put("publish", false);
+        argOptions.put("query", false);
+        argOptions.put("remove", false);
+        argOptions.put("secret", true);
+        argOptions.put("servers", true);
+        argOptions.put("share", false);
+        argOptions.put("tags", true);
+        argOptions.put("uri", true);
+    }
     
     public static void main(String[] args) {
         System.out.println("Client has started.");
 
-        //Parse CMD options
-        Options options = new Options();
-        options.addOption("PORT", true, "Server port");
-        options.addOption("IP", true, "Server IP address");
-        
+        //Parse input arguments
+        Options initOptions = new Options();
+        for (String option: argOptions.keySet()){
+            initOptions.addOption(option, argOptions.get(option), option);
+        }
         CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = null;
+        CommandLine initCmd = null;
         
         try {
-            cmd = parser.parse(options, args);
+            initCmd = parser.parse(initOptions, args);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        if (cmd.hasOption("PORT") && cmd.hasOption("IP")) {
-            port = Integer.parseInt(cmd.getOptionValue("PORT"));
-            ip = cmd.getOptionValue("IP");
+        if (initCmd.hasOption("port") && initCmd.hasOption("host")) {
+            port = Integer.parseInt(initCmd.getOptionValue("port"));
+            ip = initCmd.getOptionValue("host");
         } else {
             System.out.println("Please provide IP and PORT options");
             System.exit(0);
-        }       
-        //-----------
+        }
         
-            //Create client socket that auto-closes upon TRY exit
-            //...and connect to a server socket
-        try (Socket socket = new Socket(ip, port)){
-            
+        //Decipher command and call respective method
+        if (initCmd.hasOption("publish")) {
+            Client.PublishCmd(initCmd);
+        } else if (initCmd.hasOption("remove")) {
+            Client.RemoveCmd(initCmd);
+        } else if (initCmd.hasOption("share")) {
+            Client.ShareCmd(initCmd);
+        } else if (initCmd.hasOption("query")) {
+            Client.QueryCmd(initCmd);
+        } else if (initCmd.hasOption("fetch")) {
+            Client.FetchCmd(initCmd);
+        } else if (initCmd.hasOption("exchange")) {
+            Client.ExchangeCmd(initCmd);
+        } else {
+            System.out.println("Please use valid arguments.");
+        }
+    }
+     
+    private static void ExchangeCmd(CommandLine initCmd) {
+        // TODO Exchange command
+        
+    }
+
+    private static void QueryCmd(CommandLine initCmd) {
+        // TODO Query command
+        
+    }
+
+    private static void ShareCmd(CommandLine initCmd) {
+        // TODO Share command
+        
+    }
+
+    public static void generalReply(String request) {
+        //TODO is this still needed?
+    	try (Socket socket = new Socket(ip, port)){
             //Get I/O streams for connection
             DataInputStream input = new DataInputStream(socket.getInputStream());
             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            //send request
+            output.writeUTF(request);
+            System.out.println("request sent");
+            output.flush();
+                        
+            while(true) {
+            	if(input.available() > 0) {
+            		System.out.println(input.readUTF());
+            		break;
+            	}
+            }
             
-            //Create a JSONObject command and send it to server
-            JSONObject command = new JSONObject();
-            
-            //create a test resource
-            JSONObject resource = new JSONObject();
-            String[] tags = {"tag1", "tag2"};
-            resource.put("name", "testName");            
-            resource.put("tags", tags.toString());
-            
-            //create a test publish command
-            command.put("command", "PUBLISH");
-            command.put("resource", resource.toJSONString());
-            command.put("description", "testDescription");
-            command.put("uri", "testURI");
-            command.put("channel", "testChannel");
-            command.put("owner", "");
-            command.put("ezserver", null);
-            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @SuppressWarnings("unchecked")  
+    //JSONObject extends HashMap but does not have type parameters as HashMap would expect...
+    public static void PublishCmd(CommandLine initCmd) {
+        //TODO Publish command, remove testing code
+        
+        //Create a JSONObject command and send it to server
+        JSONObject command = new JSONObject();
+        
+        //create a test resource
+        JSONObject resource = new JSONObject();
+        String[] tags = {"tag1", "tag2"};
+        resource.put("name", "testName");            
+        resource.put("tags", tags.toString());
+        resource.put("description", "testDescription");
+        resource.put("uri", initCmd.getOptionValue("uri"));
+        resource.put("channel", "testChannel");
+        resource.put("owner", "");
+        resource.put("ezserver", null);
+        //create a test publish command
+        command.put("command", "PUBLISH");
+        command.put("resource", resource.toJSONString());
+        
+        generalReply(command.toJSONString());
+    }
+    
+    @SuppressWarnings("unchecked")
+    //JSONObject extends HashMap but does not have type parameters as HashMap would expect...
+    public static void RemoveCmd(CommandLine initCmd) {
+        //TODO Remove command, finalize and remove test code
+        
+    	//Create a JSONObject command and send it to server
+        JSONObject command = new JSONObject();
+        
+        //create a test resource
+        JSONObject resource = new JSONObject();
+        String[] tags = {"tag1", "tag2"};
+        resource.put("name", "testName");            
+        resource.put("tags", tags.toString());
+        resource.put("description", "testDescription");
+        resource.put("uri", initCmd);
+        resource.put("channel", "testChannel");
+        resource.put("owner", "");
+        resource.put("ezserver", null);
+        //create a test publish command
+        command.put("command", "REMOVE");
+        command.put("resource", resource.toJSONString());
+        
+        generalReply(command.toJSONString());
+    }
+    
+    @SuppressWarnings("unchecked")
+    //JSONObject extends HashMap but does not have type parameters as HashMap would expect...
+    public static void FetchCmd(CommandLine initCmd) {
+        //TODO Fetch command, remove test code and finalise
+        
+    	//Create a JSONObject command and send it to server
+        JSONObject command = new JSONObject();
+        
+        //create a test resource
+        JSONObject resourceTemplate = new JSONObject();
+        String[] tags = {"tag1", "tag2"};
+        resourceTemplate.put("name", "testName");            
+        resourceTemplate.put("tags", tags.toString());
+        resourceTemplate.put("description", "testDescription");
+        resourceTemplate.put("uri", initCmd);
+        resourceTemplate.put("channel", "testChannel");
+        resourceTemplate.put("owner", "");
+        resourceTemplate.put("ezserver", null);
+        //create a test publish command
+        command.put("command", "FETCH");
+        command.put("resourceTemplate", resourceTemplate.toJSONString());
+        
+        try (Socket socket = new Socket(ip, port)){
+            //Get I/O streams for connection
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            //send request
             output.writeUTF(command.toJSONString());
             System.out.println("request sent");
             output.flush();
             
-           try {
-                 String message = input.readUTF();
-                 System.out.println(message);
-            } catch (IOException e) {
-                System.out.println("Server seems to have closed connection.");
+            JSONParser JSONparser = new JSONParser();
+            
+            while(true) {
+            	if(input.available() > 0) {
+            		String result = input.readUTF();
+            		JSONObject reply = (JSONObject) JSONparser.parse(result);
+            		//the first reply: response
+            		if(reply.containsKey("response")) {
+            			if(reply.get("response").equals("success")) {
+            				continue;
+            			} else {
+            				System.out.println(reply.toJSONString());
+            				break;
+            			}
+            		}
+            		//the second reply: resource and file
+            		if(reply.containsKey("resourceSize")) {
+            			Long fileSizeRemaining = (Long) reply.get("resourceSize");            			
+            			int chunkSize = setChunkSize(fileSizeRemaining);            			
+            			byte[] receiveBuffer = new byte[chunkSize];
+            			String fileName = "clientFile/test.png";
+            			RandomAccessFile downloadingFile = new RandomAccessFile(fileName, "rw");
+            			//remaining file size
+            			int num;
+            			
+            			System.out.println("Downloading file of size" + fileSizeRemaining);
+            			while((num = input.read(receiveBuffer)) > 0) {
+            				downloadingFile.write(Arrays.copyOf(receiveBuffer, num));
+            				fileSizeRemaining -= num;
+            				
+            				chunkSize = setChunkSize(fileSizeRemaining);
+            				receiveBuffer = new byte[chunkSize];
+            				
+            				if(fileSizeRemaining == 0){
+            					break;
+            				}
+            			}
+            			System.out.println("File received!");
+            			downloadingFile.close();
+            		}
+            		//the last reply: resultSize
+            		if(reply.containsKey("resultSize")) {
+            			System.out.println(reply.toJSONString());
+            		}
+            	}
             }
-       
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
+    }
+    
+    public static int setChunkSize(long fileSizeRemaining) {
+    	int chunkSize = 1024*1024;
+    	
+    	if(fileSizeRemaining < chunkSize) {
+    		chunkSize = (int) fileSizeRemaining;
+    	}
+    	
+    	return chunkSize;
     }
 }
--- src/server/Connection.java
+++ src/server/Connection.java
@@ -1,14 +1,29 @@
+<<<<<<< HEAD
 //lisa branch
+=======
+/*
+ * Distributed Systems
+ * Group Project 1
+ * Sem 1, 2017
+ * Group: AALT
+ * 
+ * Class that handles connections with each client
+ * It runs as a separate thread
+ * It implements the major functionality expected of the server e.g. Publish, Remove, etc.
+ */
+
+>>>>>>> master
 package server;
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Connection implements Runnable {
-
 	private int id;
 	private Socket client;
 	private ResourceList resourceList;
@@ -19,7 +34,10 @@ public class Connection implements Runnable {
 		this.resourceList = resourceList;
 	}
 	
-	@Override
+	@SuppressWarnings("unchecked")
+	//JSONObject extends HashMap but does not have type parameters as HashMap would expect...
+	
+    @Override
 	public void run() {
 		System.out.println("Connection: " + id);
 		try {
@@ -27,37 +45,64 @@ public class Connection implements Runnable {
 			DataOutputStream output = new DataOutputStream(client.getOutputStream());			
 			JSONParser parser = new JSONParser();
 			
-			JSONObject command = (JSONObject) parser.parse(input.readUTF());
-			JSONObject reply = new JSONObject();
-			if(command.containsKey("command")) {
-				switch((String) command.get("command")) {
+			JSONObject client_request = (JSONObject) parser.parse(input.readUTF());
+			
+			if(client_request.containsKey("command")) {
+				switch((String) client_request.get("command")) {
 				case "PUBLISH":
-					System.out.println("enter publish");
-					System.out.println(command.toJSONString());
-					System.out.println("--------------------------");
-					reply = publish(command);
+					publish(client_request, output);
 					break;
+				case "REMOVE":
+					remove(client_request, output);
+					break;
+				case "SHARE":
+                    share(client_request, output);
+                    break;
+				case "QUERY":
+                    query(client_request, output);
+                    break;
+				case "FETCH":
+					fetch(client_request, output);
+					break;
+				case "EXCHANGE":
+                    exchange(client_request, output);
+                    break;
 				default:
+					JSONObject reply = new JSONObject();
 					reply.put("error", "unknown_command");
+					output.writeUTF(reply.toJSONString());
 				}
-
 			}
-			//put reply in output stream
-			output.writeUTF(reply.toJSONString());
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}		
 	}
 
-	private JSONObject publish(JSONObject command) throws ParseException {
+	private void exchange(JSONObject client_request, DataOutputStream output) {
+        // TODO Exchange method
+        
+    }
+
+    private void query(JSONObject client_request, DataOutputStream output) {
+        // TODO Query method
+        
+    }
+
+    private void share(JSONObject client_request, DataOutputStream output) {
+        // TODO Share method
+        
+    }
+
+    @SuppressWarnings("unchecked")
+    private void publish(JSONObject client_req, DataOutputStream output) throws ParseException, IOException {
 		JSONParser parser = new JSONParser();
-		JSONObject resourceJSON = (JSONObject) parser.parse((String) command.get("resource"));
+		JSONObject resourceJSON = (JSONObject) parser.parse((String) client_req.get("resource"));
 		
 		Resource resource = JSONObj2Resource(resourceJSON);
 		
 		boolean result = resourceList.addResource(resource);
 		
-		//make a reply
+		//create a reply
 		JSONObject reply = new JSONObject();
 		if(result) {
 			reply.put("response", "success");
@@ -65,11 +110,70 @@ public class Connection implements Runnable {
 			reply.put("reponse", "fail");
 		}
 		
-		return reply;
+		//put reply in output stream
+		System.out.println("resourceListSize: " + resourceList.getSize());
+		output.writeUTF(reply.toJSONString());
+	}
+	
+	@SuppressWarnings("unchecked")
+    private void remove(JSONObject command, DataOutputStream output) throws ParseException, IOException {
+		JSONParser parser = new JSONParser();
+		JSONObject resourceJSON = (JSONObject) parser.parse((String) command.get("resource"));
+		
+		Resource resource = JSONObj2Resource(resourceJSON);
+		boolean result = resourceList.removeResource(resource);
+		
+		//create a reply
+		JSONObject reply = new JSONObject();
+		if(result) {
+			reply.put("response", "success");
+		} else {
+			reply.put("reponse", "fail");
+		}
+		
+		//put reply in output stream
+		System.out.println("resourceListSize: " + resourceList.getSize());
+		output.writeUTF(reply.toJSONString());
+	}
+	
+	@SuppressWarnings("unchecked")
+    private void fetch(JSONObject command, DataOutputStream output) throws ParseException, IOException {
+
+	    //the following three lines not being used so far, thus commented out. Decomment when needed.
+//		JSONParser parser = new JSONParser();
+//		JSONObject resourceJSON = (JSONObject) parser.parse((String) command.get("resourceTemplate"));
+//		Resource resourceTemplate = JSONObj2Resource(resourceJSON);
+		
+		//Use a known URI, need to check the file afterward ? 
+		String URI = "serverFile/testFile.png";
+		File f = new File(URI);
+		if(f.exists()) {
+			JSONObject reponse = new JSONObject();
+			reponse.put("response", "success");
+			output.writeUTF(reponse.toJSONString());
+			
+			RandomAccessFile byteFile = new RandomAccessFile(f, "r");
+			
+			JSONObject resource = new JSONObject();
+			resource.put("resourceSize", byteFile.length());
+			output.writeUTF(resource.toJSONString());
+			
+			byte[] sendingBuffer = new byte[1024*1024];
+			int num;
+			while((num = byteFile.read(sendingBuffer)) > 0) {
+				output.write(Arrays.copyOf(sendingBuffer, num));
+			}
+			byteFile.close();
+			
+			JSONObject resultFile = new JSONObject();
+			resultFile.put("resultSize", 1);
+			output.writeUTF(resultFile.toJSONString());
+		}
+		
 	}
 	
 	private Resource JSONObj2Resource(JSONObject resource) {
-		
+		//handle default value here
 		String Name = resource.containsKey("name") ? (String) resource.get("name") : "";
 		String Description = resource.containsKey("description") ? (String) resource.get("description") : "";
 		//String[] Tags is different deal with it later
